fbcce12978bf5864278d79ab03755bcbe207150a
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2022-10-31 21:29:36 +0100

    jeh
    fixed hobbies button <3

--- script.js
+++ script.js
@@ -28,5 +28,20 @@ function onClick(element) {
   }
   
   
-  
+
+function myFunctionBtn() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Show hobbies"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Hide Hobbies"; 
+    moreText.style.display = "inline";
+  }
+}
 
