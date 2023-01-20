a6d4356ae92610f9f133e5ff6f44a8f61f629762
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-01-20 16:01:24 +0100

    hmm

--- cryptoSwap/CryptoSwap.js
+++ cryptoSwap/CryptoSwap.js
@@ -3,6 +3,7 @@
 // api from 3 diferent locations in 1inch. to get actual 10 (different tokens)
  
 async function getTop10Tokens() {
+
     const response = await fetch('https://api.coinpaprika.com/v1/coins');
     const tokens = await response.json();
 return tokens.filter(token => token.rank  >= 1 && token.rank <= 10).map(token => token.symbol);
@@ -11,6 +12,7 @@ return tokens.filter(token => token.rank  >= 1 && token.rank <= 10).map(token =>
 
 
 async function getTicerData(ticerList) {
+
    const [response1, response2, response3] = await Promise.all([
                           fetch('https://api.1inch.exchange/v3.0/1/tokens'),
                           fetch('https://api.1inch.exchange/v3.0/137/tokens'),
@@ -19,14 +21,24 @@ async function getTicerData(ticerList) {
 const tokens1 = await response1.json();
 const tokens2 = await response2.json();
 const tokens3 = await response3.json();
-const tokenList = [].concat(tokens1.tokens, tokens2.tokens, tokens3.tokens);
+const tokenList = Object.values(tokens1.tokens, tokens2.tokens, tokens3.tokens);
 return tokenList.filter(token => ticerList.includes(token.symbol));
 }
+
+function renderForm(tokens) {
+
+    const options = tokens.map(token => 
+        `<option value="${token.adress}">${token.name} (${token.symbol})</option`);
+ 
+        document.querySelector('select[name=from-token]').innerHTML = options;
+        document.querySelector('select[name=to-token]').innerHTML = options;
+        
+    }
 
 
 getTop10Tokens()
 .then(getTicerData)
-.then(console.log);
+.then(renderForm);
 
 
 
