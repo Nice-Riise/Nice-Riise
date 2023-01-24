3325ce0e97eedeb3dc04f97bdb2d37541ffcb28b
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-01-24 17:03:30 +0100

    fixed the api to just one :/

--- cryptoSwap/CryptoSwap.js
+++ cryptoSwap/CryptoSwap.js
@@ -3,63 +3,45 @@
 // api from 3 diferent locations in 1inch. to get actual 10 (different tokens)
  
 async function getTop10Tokens() {
-
-    const response = await fetch('https://api.coinpaprika.com/v1/coins');
-    const tokens = await response.json();
-return tokens.filter(token => token.rank  >= 1 && token.rank <= 10).map(token => token.symbol);
-
+  const response = await fetch('https://api.coinpaprika.com/v1/coins');
+  const tokens = await response.json();
+  return tokens.filter(token => token.rank  >= 1 && token.rank <= 10).map(token => token.symbol);
 }
 
-
 async function getTicerData(ticerList) {
-
-   const [response1, response2, response3] = await Promise.all([
-                          fetch('https://api.1inch.exchange/v3.0/1/tokens'),
-                          fetch('https://api.1inch.exchange/v3.0/137/tokens'),
-                          fetch('https://api.1inch.exchange/v3.0/56/tokens')
-]);
-const tokens1 = await response1.json();
-const tokens2 = await response2.json();
-const tokens3 = await response3.json();
-const tokenList = [...Object.values(tokens1.tokens), ...Object.values(tokens2.tokens), ...Object.values(tokens3.tokens)];
-return tokenList.filter(token => ticerList.includes(token.symbol));
+  const response = await fetch('https://api.1inch.exchange/v3.0/56/tokens');
+  const tokens = await response.json();
+  const tokenList = [...Object.values(tokens.tokens)];
+  return tokenList.filter(token => ticerList.includes(token.symbol));
 }
 
 function renderForm(tokens) {
-
-    const options = tokens.map(token => 
-        `<option value="${token.decimals}-${token.address}">${token.name} (${token.symbol})</option>`);
- 
-        document
-        .querySelector('select[name=from-token]')
-        .innerHTML = options;
-        document
-        .querySelector('select[name=to-token]')
-        .innerHTML = options;
-        
-    }
-
-async function formSubmitted(event) {
-    event.preventDefault();
-    const fromToken = document.querySelector('select[name=from-token]').value;
-    const toToken = document.querySelector('select[name=to-token]').value;
-    const [fromDecimals, fromAddress] = fromToken.split('-');
-    const [toDecimals, toAddress] = toToken.split('-');
-    const fromUnit = 10 ** fromDecimals; 
-    if(isNaN(fromUnit)){
-        console.log("amount should be a number string ");
-        return;
-    }
-    const url = `https://api.1inch.io/v5.0/1/quote?fromTokenAddress=${fromAddress}&toTokenAddress=${toAddress}&amount=${fromUnit}`;
-
-    const response = await fetch(url);
-    const quote = await response.json();
-    const exchangeRate = Number(quote.toTokenAmount) / Number(quote.fromTokenAmount);
-    document.querySelector('.js-result-quote-container').innerHTML = `
-            <h4>1 ${quote.fromToken.symbol} = ${exchangeRate} ${quote.toToken.symbol}</h4>
-    `;
+  const options = tokens.map(token => 
+      `<option value="${token.decimals}-${token.address}">${token.name} (${token.symbol})</option>`);
+  document.querySelector('select[name=from-token]').innerHTML = options;
+  document.querySelector('select[name=to-token]').innerHTML = options;
 }
 
+
+async function formSubmitted(event) {
+  event.preventDefault();
+  const fromToken = document.querySelector('select[name=from-token]').value;
+  const toToken = document.querySelector('select[name=to-token]').value;
+  const [fromDecimals, fromAddress] = fromToken.split('-');
+  const [toDecimals, toAddress] = toToken.split('-');
+  const fromUnit = 10 ** fromDecimals; 
+  if(isNaN(fromUnit)){
+      console.log("amount should be a number string ");
+      return;
+  }
+  const url = `https://api.1inch.io/v3.0/56/quote?fromTokenAddress=${fromAddress}&toTokenAddress=${toAddress}&amount=${fromUnit}`;
+  const response = await fetch(url);
+  const quote = await response.json();
+  const exchangeRate = Number(quote.toTokenAmount) / Number(quote.fromTokenAmount);
+  document.querySelector('.js-result-quote-container').innerHTML = `
+          <h4>1 ${quote.fromToken.symbol} = ${exchangeRate} ${quote.toToken.symbol}</h4>
+  `;
+}
 
     document
     .querySelector('button.SwapPrice')
@@ -87,6 +69,5 @@ function openMenu(evt, menuName) {
   evt.currentTarget.firstElementChild.className += " w3-dark-grey";
 }
 document.getElementById("myLink").click();
-
 
 
