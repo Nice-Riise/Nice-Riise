bf0080cefcc287c99e3b486c00f442bddafbfc85
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-01-24 17:23:50 +0100

    top 40 coins added

--- cryptoSwap/CryptoSwap.js
+++ cryptoSwap/CryptoSwap.js
@@ -5,7 +5,7 @@
 async function getTop10Tokens() {
   const response = await fetch('https://api.coinpaprika.com/v1/coins');
   const tokens = await response.json();
-  return tokens.filter(token => token.rank  >= 1 && token.rank <= 10).map(token => token.symbol);
+  return tokens.filter(token => token.rank  >= 1 && token.rank <= 40).map(token => token.symbol);
 }
 
 async function getTicerData(ticerList) {
