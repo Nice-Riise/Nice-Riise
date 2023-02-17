7e6c4efcf5730667d2deefad2f04fe6cadf90d17
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-02-17 19:15:30 +0100

    yes

---
+++ chatGPT/chatGPT.js
@@ -0,0 +1,60 @@
+const chatContainer = document.getElementById("chat-window");
+const chatForm = document.getElementById("chat-form");
+const chatInput = document.getElementById("chat-input");
+
+const apiKey = "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";
+
+function sendMessage(message) {
+  const messageElement = addMessage(message, "user", "pending");
+  
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: message,
+      max_tokens: 100,
+      n: 1,
+      stop: ["\n"]
+    })
+  };
+
+  fetch("https://api.openai.com/v1/engines/davinci-codex/completions", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      const response = data.choices[0].text.trim();
+      updateMessageStatus(messageElement, "sent");
+      addMessage(response, "bot");
+    })
+    .catch(error => {
+      console.error(error);
+      updateMessageStatus(messageElement, "failed");
+    });
+}
+
+function addMessage(message, sender, status) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("message");
+  messageElement.classList.add(sender);
+  messageElement.classList.add(status);
+  messageElement.innerText = message;
+  chatContainer.appendChild(messageElement);
+  
+  return messageElement;
+}
+
+function updateMessageStatus(messageElement, status) {
+  messageElement.classList.remove("pending");
+  messageElement.classList.add(status);
+}
+
+chatForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const message = chatInput.value.trim();
+  if (message) {
+    sendMessage(message);
+    chatInput.value = "";
+  }
+});
