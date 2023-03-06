f7db016f9531d7492bec61ac8baff0fc5291eb92
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-03-06 14:18:15 +0100

    alot of canges .
    implementing a working chatbot

--- RiiseChat/RiiseChat.js
+++ RiiseChat/RiiseChat.js
@@ -1,18 +1,8 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-      
-// Retrieve API key from environment variable
-const apiKey = process.env.OPENAI_API_KEY;
-
-// Check if API key is set
-if (!apiKey) {
-  console.error('Error: OPENAI_API_KEY environment variable not set.');
-  process.exit(1);
-}
-
-const chatWindow = document.getElementById("messages");
-const inputField = document.getElementById("input");
+const apiKey = '';
+const chatWindow = document.getElementById('messages');
+const inputField = document.getElementById('input');
+const submitButton = document.getElementById('submit-button');
+const thinkingImage = document.querySelector('.thinking-image');
 
 function sendMessage(input) {
   let requestOptions = {
@@ -32,37 +22,72 @@ function sendMessage(input) {
   };
 
   addChatEntry("user", input);
-  addChatEntry("bot", "Typing...");
+  addChatEntry("BotoCop", "BotoCop is typing...");
+  showThinkingImage(); // show the thinking image
 
   fetch("https://api.openai.com/v1/engines/davinci-codex/completions", requestOptions)
     .then(response => response.json())
     .then(data => {
       let product = data.choices[0].text;
       product = product.slice(0, product.lastIndexOf(".")) + ".";
-      addChatEntry("bot", product);
+      let humanWords = product.split(" ").filter(word => !word.includes('\x07')).join(" ");
+      addChatEntry("BotoCop", humanWords);
+      setTimeout(() => {
+        hideThinkingImage(); // hide the thinking image
+      }, 2000); // hide the thinking image after 2 seconds
+
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      console.error(error);
+      addChatEntry("BotoCop", "Sorry, I'm resting. Please try again later.");
+      hideThinkingImage(); // hide the thinking image in case it's still showing
+    });
 }
 
 function addChatEntry(sender, message) {
-  const messagesContainer = document.getElementById("messages");
+  const messagesContainer = document.getElementById('messages');
 
-  let chatDiv = document.createElement("div");
+  let chatDiv = document.createElement('div');
   chatDiv.className = `${sender} response`;
   chatDiv.innerHTML = `<span>${message}</span>`;
+  
+  if (sender === "user") {
+    chatDiv.classList.add("userColor");
+  } else if (sender === "BotoCop") {
+    chatDiv.classList.add("roboColor");
+  }
+  
   messagesContainer.appendChild(chatDiv);
-
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
-inputField.addEventListener("keydown", (event) => {
-  if (event.code === "Enter") {
+
+inputField.addEventListener('keydown', (event) => {
+  if (event.code === 'Enter') {
     event.preventDefault();
     let input = inputField.value.trim();
     if (input) {
       sendMessage(input);
-      inputField.value = "";
+      inputField.value = '';
     }
   }
 });
 
+// Initial bot message
+addChatEntry('BotoCop', '#$%...YO! My Name Is BotoCop! How can I help you?');
+
+function showThinkingImage() {
+  thinkingImage.style.display = 'block';
+}
+
+function hideThinkingImage() {
+  thinkingImage.style.display = 'none';
+}
+
+//button click
+submitButton.addEventListener('click', () => {
+  submitButton.classList.add('clicked');
+  setTimeout(() => {
+    submitButton.classList.remove('clicked');
+  }, 500);
+});
