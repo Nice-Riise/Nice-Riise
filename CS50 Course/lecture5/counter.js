a0b8f7a063e2afbb72f5ccf566c3425d08c75e21
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-03-09 17:33:38 +0100

    f

--- CS50 Course/lecture5/counter.js
+++ CS50 Course/lecture5/counter.js
@@ -1,6 +1,7 @@
 
 
 
+<<<<<<< Updated upstream
 
 
 if (!localStorage.getItem("counter")) {
@@ -17,4 +18,18 @@ function count() {
 document.addEventListener("DOMContentLoaded", function() {
    document.querySelector("h1").innerHTML = localStorage.getItem("counter");
     document.querySelector("button").onclick = count;       
+=======
+let counter = 0;
+
+function count() {
+    counter++;
+    document.querySelector("h1").innerHTML = counter;
+
+    if (counter % 10 === 0) {
+        alert(`the count is now ${counter}`);
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("button").onclick = count;
+>>>>>>> Stashed changes
 }); 
