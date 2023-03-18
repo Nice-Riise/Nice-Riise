41aca8e9eeb95c06065173d8fa294a3ab5118bac
Nice-Riise <112577450+Nice-Riise@users.noreply.github.com>
2023-03-18 08:33:09 +0100

    yes project0 finished

--- CS50 Course/Lecture0/noodle.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('#search-form').submit(function(event) {
-		event.preventDefault();
-		var query = $('#search-box').val();
-		window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query);
-	});
-});
